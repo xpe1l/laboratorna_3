bb79421651b3be494900147aae65ad025585c970
xpe1l <semenuk.boghdan@gmail.com>
2023-01-04 00:43:02 +0200

    laba 3

---
+++ lab 3/lab 3/lab 3.cpp
@@ -0,0 +1,17 @@
+﻿// дослідження операторів
+#include <iostream>
+
+using namespace std;
+int main() {
+	int password = 1234, numbers;
+	cout << "enter password" << endl;
+	cin >> numbers;
+
+	if (numbers == password) {
+		cout << "the password is correct";
+	}
+	else if (numbers > password || numbers < password)
+		cout << "you entered an incorrect password";
+
+	return 0;
+}
